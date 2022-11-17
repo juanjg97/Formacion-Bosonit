b38bd6b66088354798fc46549aee539e597594d6
juanjg97 <juanjosejg2711@gmail.com>
2022-11-16 20:03:13 -0600

    Se agrega parte del ejercicio1

--- bloque-1-jdk/block1-process-file-and-streams/src/main/java/org/example/Main.java
+++ bloque-1-jdk/block1-process-file-and-streams/src/main/java/org/example/Main.java
@@ -1,7 +1,33 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String file = "bloque-1-jdk/block1-process-file-and-streams/src/fichero.csv";
+        BufferedReader reader = null;
+        String line = "";
+
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            while((line=reader.readLine()) != null){
+                String[] row = line.split(":");
+                for(String index:row){
+                    System.out.printf("%-10s",index);
+                }
+                System.out.println();
+            }
+        }catch(Exception e) {
+            e.printStackTrace();
+        }finally{
+            try{
+                reader.close();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
     }
 }
