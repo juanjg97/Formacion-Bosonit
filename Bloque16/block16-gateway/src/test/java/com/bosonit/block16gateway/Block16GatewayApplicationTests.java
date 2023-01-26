ffeec91f581036facb544b714ca12682f5f445bb
juanjg97 <juanjosejg2711@gmail.com>
2023-01-26 10:02:19 -0600

    Block16 - Parte 4 completa, se implementa api-gateway

---
+++ Bloque16/block16-gateway/src/test/java/com/bosonit/block16gateway/Block16GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosonit.block16gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Block16GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- Bloque16/block16-ticket/src/main/java/com/bosonit/block16ticket/Block16TicketApplication.java
+++ Bloque16/block16-ticket/src/main/java/com/bosonit/block16ticket/Block16TicketApplication.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
 public class Block16TicketApplication implements CommandLineRunner {
